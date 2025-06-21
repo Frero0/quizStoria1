3be278076d3e24034480e7c67ff9bd7304f90c40
federicosantorsola <fede.uchi@icloud.com>
2025-06-21 09:35:09 +0200

    add economia quiz

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <header className="w-full bg-white shadow py-4">
             <div className="max-w-2xl mx-auto px-4">
-                <h1 className="text-2xl font-bold text-purple-700">Quiz di Storia dell’Informatica</h1>
+                <h1 className="text-2xl font-bold text-purple-700">Quiz di Macro Economia Unito</h1>
             </div>
         </header>
     );
